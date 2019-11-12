9749faf1c8f460cf4566580281814099f14688d3
totolinv <57598334+totolinv@users.noreply.github.com>
2019-11-12 21:44:20 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,1 +1,17 @@
-
+'''Cinema module'''
+import requests
+from bs4 import BeautifulSoup
+class CinemaParser:
+    '''class'''
+    def __init__(self, city='msk'):
+        ''' Узнаём город'''
+        self.city = city
+        self.content = ''
+    def extract_raw_content(self):
+        '''скачивает Html'''
+        url = 'https://{}.subscity.ru'.format(self.city)
+        self.content = requests.get(url)
+    def print_raw_content(self):
+        '''Красивый вывод HTML'''
+        soup = BeautifulSoup(self.content.text, 'html.parser')
+        return soup.prettify()
